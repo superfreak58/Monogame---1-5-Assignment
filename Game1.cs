2c2e1861aaec3bdbdaa72735815879a219f7af2b
superfreak58 <rayjkolyn@gmail.com>
2024-11-27 09:17:57 -0500

    so skib

--- Game1.cs
+++ Game1.cs
@@ -5,6 +5,11 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Monogame___1_5
 {
+    public enum Screen
+    {
+        Intro,
+        Game
+    }
 
     public class Game1 : Game
     {
@@ -12,11 +17,15 @@ namespace Monogame___1_5
         private SpriteBatch _spriteBatch;
         Rectangle field, vikesWR1, football, window;
         Vector2 vikeSpeed, ballSpeed;
-        Texture2D vikesHelmet, saintsHelmet, fieldTexture, footballTexture, touchdownTexture;
+        Texture2D vikesHelmet, saintsHelmet, fieldTexture, footballTexture, touchdownTexture, introTexture;
         SoundEffect commentary;
         double seconds;
+        Screen screen = Screen.Intro;
 
-        
+
+
+
+
 
         MouseState mouseState;
 
@@ -51,82 +60,107 @@ namespace Monogame___1_5
             fieldTexture = Content.Load<Texture2D>("field");
             footballTexture = Content.Load<Texture2D>("football");
             commentary = Content.Load<SoundEffect>("commentary14");
-            touchdownTexture = Content.Load<Texture2D>("touchdown");
+            touchdownTexture = Content.Load<Texture2D>("touchdown (1)");
+            introTexture = Content.Load<Texture2D>("intro screen football");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            
-            if (seconds <= 0)
-                commentary.Play();
-            mouseState = Mouse.GetState();
+            MouseState mouseState = Mouse.GetState();
 
-            this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}";
 
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-            vikesWR1.X += (int)vikeSpeed.X;
-            vikesWR1.Y += (int)vikeSpeed.Y;
-            football.X += (int)ballSpeed.X;
-            football.Y += (int)ballSpeed.Y;
-            if (vikesWR1.Y <= 170)
+
+
+
+
+            if (screen == Screen.Intro)
             {
-               
-                ballSpeed = new Vector2(4, -3);
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.Game;
+            }
+            if (screen == Screen.Game)
+            {
+                if (seconds <= 0)
+                    commentary.Play();
+                mouseState = Mouse.GetState();
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
+                vikesWR1.X += (int)vikeSpeed.X;
+                vikesWR1.Y += (int)vikeSpeed.Y;
+                football.X += (int)ballSpeed.X;
+                football.Y += (int)ballSpeed.Y;
+                if (vikesWR1.Y <= 170)
+                {
+
+                    ballSpeed = new Vector2(4, -3);
+
+                }
+                if (vikesWR1.Top < 0)
+                {
+                    vikeSpeed = new Vector2(0, 0);
+
+                }
+                if (vikesWR1.Contains(football))
+                {
+                    ballSpeed.X = vikeSpeed.X;
+                    ballSpeed.Y = vikeSpeed.Y;
+
+                }
+                if (football.Y >= 196 && football.Y <= 296)
+                {
+                    football = new Rectangle(football.X, football.Y, 50, 50);
+
+                }
+                else
+                {
+                    football = new Rectangle(football.X, football.Y, 25, 25);
+                }
+
+                // TODO: Add your update logic here
+
+                base.Update(gameTime);
+
+                seconds += gameTime.ElapsedGameTime.TotalSeconds;
 
             }
-            if (vikesWR1.Top <0)
-            {
-                vikeSpeed = new Vector2(0, 0);
-
-            }
-            if (vikesWR1.Contains(football))
-            {
-                ballSpeed.X = vikeSpeed.X;
-                ballSpeed.Y = vikeSpeed.Y;
-
-            }
-            if (football.Y >= 196 && football.Y<=296) 
-            {
-                football = new Rectangle(football.X, football.Y,50,50);
-                
-            }
-            else 
-            {
-                football = new Rectangle(football.X, football.Y, 25, 25);
-            }
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-
-            seconds += gameTime.ElapsedGameTime.TotalSeconds;
-            
         }
 
-        protected override void Draw(GameTime gameTime)
+        
+           protected override void Draw(GameTime gameTime)
         {
+            _spriteBatch.Begin();
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-            _spriteBatch.Begin();
+            // Check if WR is off-screen
+           
+
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(introTexture, window, Color.White);
+            }
+            else if (screen == Screen.Game)
+            {
+                _spriteBatch.Draw(fieldTexture, window, Color.White);
+                _spriteBatch.Draw(vikesHelmet, new Rectangle(361, 345, 50, 50), Color.White);
+                _spriteBatch.Draw(vikesHelmet, new Rectangle(215, 288, 50, 50), Color.White);
+                _spriteBatch.Draw(vikesHelmet, new Rectangle(290, 288, 50, 50), Color.White);
+                _spriteBatch.Draw(vikesHelmet, new Rectangle(365, 288, 50, 50), Color.White);
+                _spriteBatch.Draw(vikesHelmet, new Rectangle(440, 288, 50, 50), Color.White);
+                _spriteBatch.Draw(vikesHelmet, new Rectangle(515, 288, 50, 50), Color.White);
+                _spriteBatch.Draw(vikesHelmet, vikesWR1, Color.White);
+                _spriteBatch.Draw(footballTexture, football, Color.White);
+            }
             if (vikesWR1.Top < 0)
             {
-                _spriteBatch.Draw(touchdownTexture, new Rectangle(0, 0, 500, 800), Color.White);
-
+                _spriteBatch.Draw(touchdownTexture, new Rectangle(0,0,800,500), Color.White);
             }
-            _spriteBatch.Draw(fieldTexture, window, Color.White);
-            _spriteBatch.Draw(vikesHelmet, new Rectangle(361, 345, 50, 50), Color.White);
-            _spriteBatch.Draw(vikesHelmet, new Rectangle(215, 288, 50, 50), Color.White);
-            _spriteBatch.Draw(vikesHelmet, new Rectangle(290, 288, 50, 50), Color.White);
-            _spriteBatch.Draw(vikesHelmet, new Rectangle(365, 288, 50, 50), Color.White);
-            _spriteBatch.Draw(vikesHelmet, new Rectangle(440, 288, 50, 50), Color.White);
-            _spriteBatch.Draw(vikesHelmet, new Rectangle(515, 288, 50, 50), Color.White);
-            _spriteBatch.Draw(vikesHelmet,vikesWR1, Color.White);
-            _spriteBatch.Draw(footballTexture,football, Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
     }
 }
+    
+
